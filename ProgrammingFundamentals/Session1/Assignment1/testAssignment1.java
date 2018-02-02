f7fd746df5862859b3325a66a6a168e69aab3881
meta-vinod-patel <35729011+meta-vinod-patel@users.noreply.github.com>
2018-02-02 12:41:52 +0530

    Update testAssignment1.java

--- ProgrammingFundamentals/Session1/Assignment1/testAssignment1.java
+++ ProgrammingFundamentals/Session1/Assignment1/testAssignment1.java
@@ -7,20 +7,18 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestAssignment {
-
-	Assignment1 obj = new Assignment1();
 	
 	//to test the actual result and expected result	
 	@Test
 	public void maintest() {
-		int binary = obj.binary_to_octal(11001000);
+		int binary = Assignment1.binary_to_octal(11001000);
 		assertEquals(310,binary);
 	}
 	
 	//to test the actual result and expected result for allzero
 	@Test
 	public void allZero() {
-		assertEquals(0, obj.binary_to_octal(000));
+		assertEquals(0, Assignment1.binary_to_octal(000));
 	}
 
 }
